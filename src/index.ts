a8c48cf72e91e1b298c87b50bec86d5fdf60000d
greatpeak <abatandivine@gmail.com>
2025-01-02 22:54:13 +0100

    okay

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(5000, () => {
+  console.log("server 5000");
 });
